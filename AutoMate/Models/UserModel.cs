0c9f9cc2ae48408c2e155d0034904ee8f7db8a89
Jesse Gray <jessegray13@hotmail.com>
2019-09-07 09:40:55 +1200

    Made UserModel

---
+++ AutoMate/Models/UserModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoMate.Models
+{
+    public class UserModel
+    {
+        public string GivenName { get; set; }
+
+        public string FamilyName { get; set; }
+
+        public string MobileNumber { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public int UserID { get; set; }
+
+        public bool DriverVerification { get; set; }
+
+        public string UserName { get; set; }
+
+        public double Rating { get; set; }
+
+    }
+}
