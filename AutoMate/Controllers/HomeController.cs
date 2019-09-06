953dd1b060648f4a4fa1328f2d5255a6dfd0287e
catokass <kassian.houben@gmail.com>
2019-09-07 09:19:45 +1200

    Check if I can commit

--- AutoMate/Controllers/HomeController.cs
+++ AutoMate/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace AutoMate.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page."; 
 
             return View();
         }
