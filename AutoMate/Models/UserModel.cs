cb21f7a5d4614571045e2c3d4316d4ff5effbba9
Jesse Gray <jessegray13@hotmail.com>
2019-09-07 10:01:55 +1200

    Made UserModel

--- AutoMate/Models/UserModel.cs
+++ AutoMate/Models/UserModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,19 +8,36 @@ namespace AutoMate.Models
 {
     public class UserModel
     {
+        [Required(ErrorMessage = "You need to provide a given name")]
+        [Display(Name = "Given Name")]
         public string GivenName { get; set; }
 
+        [Required(ErrorMessage = "You need to provide a family name")]
+        [Display(Name = "Family Name")]
         public string FamilyName { get; set; }
 
+        [Required(ErrorMessage = "You need to provide a mobile number")]
+        [Display(Name = "Mobile Number")]
         public string MobileNumber { get; set; }
 
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "You need to provide an email address")]
+        [Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
 
+        [DataType(DataType.EmailAddress)]
+        [Compare("EmailAddress", ErrorMessage = "The emails must match")]
+        [Display(Name = "Confirm Email Address")]
+        public string ConfirmEmailAddress { get; set; }
+
+        [Required(ErrorMessage = "You need to provide a User ID")]
+        [Display(Name = "User ID")]
         public int UserID { get; set; }
 
-        public bool DriverVerification { get; set; }
+        public bool DriverVerification { get; set; } = true;
 
-        public string UserName { get; set; }
+        [Required(ErrorMessage = "You need to provide a username")]
+        public string Username { get; set; }
 
         public double Rating { get; set; }
 
