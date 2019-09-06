e3af5128872158164fe1b2c8141f90e9a440dccf
keezeden <keezeden@LAPTOP-L0KN4ACR>
2019-09-07 06:23:41 +1200

    Added Map functionality and started pulling data from api

--- AutoMate/App_Start/RouteConfig.cs
+++ AutoMate/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace AutoMate
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Map",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Map", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
