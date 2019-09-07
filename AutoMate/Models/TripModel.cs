d4270ed9fe83f87404908769c8eb6d487e8d6541
sjdolley <garfunkalman2@gmail.com>
2019-09-07 15:30:42 +1200

    Added Position Variables

--- AutoMate/Models/TripModel.cs
+++ AutoMate/Models/TripModel.cs
@@ -4,26 +4,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Device.Location.GeoCoordinate;
 
 // Model for Trip
 
 namespace AutoMate.Models
 {
 
-public class Trip {
-
-    
-    private int UserID { get; set; }
-    private Double StartLat { get; set; }
-    private Double StartLong { get; set; }
-    private Double EndLat { get; set; }
-    private Double EndLong { get; set; }
-    private DateTime StartTime { get; set; }
-    private DateTime EndTime { get; set; }
-    
-
-}
+    public class Trip
+    {
 
 
+        private int UserID { get; set; }
+        private Double StartLat { get; set; }
+        private Double StartLong { get; set; }
+        private Double EndLat { get; set; }
+        private Double EndLong { get; set; }
+        private DateTime StartTime { get; set; }
+        private DateTime EndTime { get; set; }
+        private Position Initial { get; set; }
+        private Position Final { get; set; }
+
+    }
 }
